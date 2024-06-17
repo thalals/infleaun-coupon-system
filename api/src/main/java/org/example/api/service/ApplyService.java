e463e5427a4db0897c78ea553fe94dac8c642143
thalals <thalsal@naver.com>
2024-06-17 17:38:13 +0900

    ✨쿠폰 프로젝트

---
+++ api/src/main/java/org/example/api/service/ApplyService.java
@@ -0,0 +1,25 @@
+package org.example.api.service;
+
+import org.example.api.domain.Coupon;
+import org.example.api.repository.CouponRepository;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ApplyService {
+
+    private final CouponRepository couponRepository;
+
+    public ApplyService(CouponRepository couponRepository) {
+        this.couponRepository = couponRepository;
+    }
+
+    public void apply(final Long userId) {
+        long count = couponRepository.count();
+
+        if (count > 100) {
+            return;
+        }
+
+        couponRepository.save(new Coupon(userId));
+    }
+}
---
+++ api/src/test/java/org/example/api/service/ApplyServiceTest.java
@@ -0,0 +1,53 @@
+package org.example.api.service;
+
+import org.assertj.core.api.Assertions;
+import org.example.api.repository.CouponRepository;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+@SpringBootTest
+class ApplyServiceTest {
+
+    @Autowired
+    private ApplyService applyService;
+
+    @Autowired
+    private CouponRepository couponRepository;
+
+    @Test
+    public void 한번만응모() {
+        applyService.apply(1L);
+        long count = couponRepository.count();
+
+        Assertions.assertThat(count).isOne();
+    }
+
+    @Test
+    public void 여러명응모_실패() throws InterruptedException {
+
+        int threadCount = 1000;
+        ExecutorService executorService = Executors.newFixedThreadPool(32);
+        CountDownLatch countDownLatch = new CountDownLatch(threadCount);
+
+        for (int i = 0; i < threadCount; i++) {
+            long userId = i;
+            executorService.execute(() -> {
+                try {
+                    applyService.apply(userId);
+                }finally {
+                    countDownLatch.countDown();
+                }
+            });
+        }
+
+        countDownLatch.await();
+
+        long count = couponRepository.count();
+        Assertions.assertThat(count).isEqualTo(100);
+    }
+}
